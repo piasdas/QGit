6f4a6f904ea0334dc741437cce414d239a6ef49b
piasxp <piasxp@gmail.com>
2019-03-29 12:34:30 +0600

    modified test.java

--- test.java
+++ test.java
@@ -1,1 +1,7 @@
 test.java  test1.txt
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
